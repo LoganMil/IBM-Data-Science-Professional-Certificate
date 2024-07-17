9c9d9b50c590f40abd9a3213d490c3f8afe0659c
LoganMil <lmpirates61@gmail.com>
2024-07-17 17:23:56 -0400

    finished lab 0423

--- 04_PythonForDSAInDEV/main.py
+++ 04_PythonForDSAInDEV/main.py
@@ -1,13 +1,13 @@
-import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
-# # clean table
-# url = 'https://en.wikipedia.org/wiki/List_of_largest_banks'
-# tables = pd.read_html(url)
-# df = tables[0]
-# print(df.head())
+url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-SkillsNetwork/labs/datasets/HTMLColorCodes.html'
 
-# messy table (note: footnotes still present)
-URL = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)'
-tables = pd.read_html(URL)
-df = tables[2] # the required table will have index 2
-print(df.head())
+data = requests.get(url).text
+soup = BeautifulSoup(data,'html5lib')
+
+table = soup.find('table')
+
+for row in table.find_all('tr'):
+    cols = row.find_all('td')
+    color_name = 
